54279338130d3db031cb9ab3de3e51b203d2b472
Azimjon-ibn-Mehmonali <azimjon.ibn.mehmonali@gmail.com>
2019-10-06 12:14:27 +0500

    qo'shish to'liq

--- integer/integer.hpp
+++ integer/integer.hpp
@@ -4,32 +4,44 @@ using namespace std;
 #define htor(s) ((s) - 48)
 #define rtoh(s) ((s) + 48)
 
-void toldir(string &s, int n)
+string add(string, string);
+void noltozala(string&);
+
+void noltozala(string & s)
 {
-    while (s.size() < n)
-        s = "0" + s;
+    int ns = 0;
+    while (s[ns] == '0')
+        ns++;
+    s.erase(0, ns);
 }
 
 string add(string a, string b)
 {
-    int ml = max(a.size(), b.size());
-
-    toldir(a, ml);
-    toldir(b, ml);
-
-    string c(ml, '0');
+    if (a.size() < b.size())
+        return add(b, a);
     
-    int yodda = 0, ry;
-    for (int i = ml - 1; i >= 0; i--)
+    a = "0" + a;
+
+    int i, j;
+    i = a.size() - 1;
+    j = b.size() - 1;
+    
+    for (; j >= 0; i--, j--)
     {
-        ry = htor(a[i]) + htor(b[i]) + yodda;
-        c[i] = rtoh(ry % 10);
-        yodda = ry / 10;
+        a[i] += htor(b[j]);
     }
-    
-    if (yodda)
-        c = "0" + c;
-        c[0] = rtoh(yodda);
 
-    return c;
+    i = a.size() - 1;
+
+    for (; i > 0; i--)
+    {
+        if (htor(a[i]) > 9)
+        {
+            a[i] -= 10;
+            a[i - 1]++;
+        }
+    }
+    noltozala(a);
+
+    return a;
 }
